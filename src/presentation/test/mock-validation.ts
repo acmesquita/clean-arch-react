fea1f058239494de521075ab077602ec5445e0b2
Catharina Mesquita <catha.ana.1994@gmail.com>
2021-10-28 17:06:17 -0300

    feat: ensure Login present error if password is invalid

--- src/presentation/test/mock-validation.ts
+++ src/presentation/test/mock-validation.ts
@@ -1,14 +1,9 @@
 import { Validation } from '@/presentation/protocols/validation'
 
-export class ValidationSpy implements Validation {
+export class ValidationStub implements Validation {
   errorMessage: string
-  fieldName: string
-  fieldValue: string
 
   validate (fieldName: string, fieldValeu: string): string {
-    this.fieldName = fieldName
-    this.fieldValue = fieldValeu
-
     return this.errorMessage
   }
 }
