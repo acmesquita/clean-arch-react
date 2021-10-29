07f877b3f71887d4b276ef1a7d7586846693dff0
Catharina Mesquita <catha.ana.1994@gmail.com>
2021-10-29 15:31:49 -0300

    feat: prevent Login to submit form is its invalid

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -30,7 +30,7 @@ const Login: React.FC<Props> = ({ validation, authentication }: Props) => {
 
   async function handleSubmit (event: React.FormEvent<HTMLFormElement>): Promise<void> {
     event.preventDefault()
-    if (state.isLoading) return
+    if (state.isLoading || state.emailError || state.passwordError) return
 
     setState(rest => ({
       ...rest,
@@ -46,7 +46,7 @@ const Login: React.FC<Props> = ({ validation, authentication }: Props) => {
     <div className={styles.loginWrapper}>
       <Header />
       <FormLoginContext.Provider value={{ state, setState }}>
-        <form className={styles.form} onSubmit={handleSubmit}>
+        <form data-testid="form" className={styles.form} onSubmit={handleSubmit}>
           <h2>Login</h2>
           <Input type="email" name="email" placeholder="Digite seu e-mail" />
           <Input type="password" name="password" id="password" placeholder="Digite sua senha" />
