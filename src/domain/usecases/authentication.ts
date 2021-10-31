e1c958cdaaf7220678b77d0840873959e0b68bc9
Catharina Mesquita <catha.ana.1994@gmail.com>
2021-10-31 15:21:16 -0300

    fix: adjust catch error AxiosHttpClient

--- src/domain/usecases/authentication.ts
+++ src/domain/usecases/authentication.ts
@@ -6,5 +6,5 @@ export type AuthenticationParams = {
 }
 
 export interface Authentication {
-  auth: (params: AuthenticationParams) => Promise<AccountModel> | null
+  auth: (params: AuthenticationParams) => Promise<AccountModel>
 }
--- src/infra/http/axios-http-client/axios-http-client.ts
+++ src/infra/http/axios-http-client/axios-http-client.ts
@@ -3,11 +3,18 @@ import axios from 'axios'
 
 export class AxiosHttpClient implements HttpPostClient<any, any> {
   async post (params: HttpPostClientParams<any>): Promise<HttpResponse<any>> {
-    const httpResponse = await axios.post(params.url, params.body)
+    try {
+      const httpResponse = await axios.post(params.url, params.body)
 
-    return {
-      statusCode: httpResponse.status,
-      body: httpResponse.data
+      return {
+        statusCode: httpResponse.status,
+        body: httpResponse.data
+      }
+    } catch (error) {
+      return {
+        statusCode: error.response.status,
+        body: error.response.data.error
+      }
     }
   }
 }
