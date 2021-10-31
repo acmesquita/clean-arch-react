57727d4247bc75c64f51d7407336d707daab0d20
Catharina Mesquita <catha.ana.1994@gmail.com>
2021-10-31 13:37:13 -0300

    refactor: ensure use random values

--- src/validation/validators/validation-composite/validation-composite.spec.ts
+++ src/validation/validators/validation-composite/validation-composite.spec.ts
@@ -1,3 +1,4 @@
+import faker from 'faker'
 import { FieldValidationSpy } from '../test/mock-field-validation'
 import { ValidationComposite } from './validation-composite'
 
@@ -6,10 +7,10 @@ type SutTypes = {
   fieldValidations: FieldValidationSpy[]
 }
 
-const makeSut = (): SutTypes => {
+const makeSut = (fieldName: string): SutTypes => {
   const fieldValidations = [
-    new FieldValidationSpy('any_field'),
-    new FieldValidationSpy('any_field')
+    new FieldValidationSpy(fieldName),
+    new FieldValidationSpy(fieldName)
   ]
 
   const sut = new ValidationComposite(fieldValidations)
@@ -22,18 +23,20 @@ const makeSut = (): SutTypes => {
 
 describe('ValidationComposite', () => {
   test('Should return error any validation fails', () => {
-    const { sut, fieldValidations } = makeSut()
-    fieldValidations[0].error = new Error('first_error_message')
-    fieldValidations[1].error = new Error('second_error_message')
+    const fieldName = faker.database.column()
+    const { sut, fieldValidations } = makeSut(fieldName)
+    const errorMessage = faker.random.words()
+    fieldValidations[0].error = new Error(errorMessage)
+    fieldValidations[1].error = new Error(faker.random.words())
 
-    const error = sut.validate('any_field', 'any_value')
-    expect(error).toBe('first_error_message')
+    const error = sut.validate(fieldName, faker.random.word())
+    expect(error).toBe(errorMessage)
   })
 
   test('Should return falsy if validation returns falsy', () => {
-    const { sut } = makeSut()
-
-    const error = sut.validate('any_field', 'any_value')
+    const fieldName = faker.database.column()
+    const { sut } = makeSut(fieldName)
+    const error = sut.validate(fieldName, faker.random.word())
     expect(error).toBeFalsy()
   })
 })
