d0a05806d2603bb6533d68cfa4bcec223552ee20
Catharina Mesquita <catha.ana.1994@gmail.com>
2021-10-31 12:40:14 -0300

    test: ensure EmailValidation return falsy if email is valid

--- src/validation/validators/email/email-validation.spec.ts
+++ src/validation/validators/email/email-validation.spec.ts
@@ -1,10 +1,17 @@
+import faker from 'faker'
 import { InvalidFieldError } from '@/validation/errors'
 import { EmailValidation } from './email-validation'
 
 describe('EmailValidation', () => {
   test('Should return error if email is invalid', () => {
     const sut = new EmailValidation('email')
-    const error = sut.validate('any_email')
+    const error = sut.validate(faker.random.word())
     expect(error).toEqual(new InvalidFieldError('email'))
+  })
+
+  test('Should return falsy if email is valid', () => {
+    const sut = new EmailValidation((faker.random.word()))
+    const result = sut.validate(faker.internet.email())
+    expect(result).toBeFalsy()
   })
 })
