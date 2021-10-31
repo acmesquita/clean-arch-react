aa7702d5f181e244f98ba6d9c6f36e74445b8205
Catharina Mesquita <catha.ana.1994@gmail.com>
2021-10-31 12:54:12 -0300

    test: ensure MinLenghtValidation returns falsy if field is valid

--- src/validation/validators/min-lenght/min-lenght-validation.spec.ts
+++ src/validation/validators/min-lenght/min-lenght-validation.spec.ts
@@ -4,8 +4,13 @@ import { MinLenghtValidation } from './min-lenght-validation'
 describe('MinLenghtValidation', () => {
   test('Should return error if field invalid', () => {
     const sut = new MinLenghtValidation('field', 5)
-    const error = sut.validate('123')
-
+    const error = sut.validate('1234')
     expect(error).toEqual(new MinLenghtError())
+  })
+
+  test('Should return falsy if field valid', () => {
+    const sut = new MinLenghtValidation('field', 5)
+    const error = sut.validate('12345')
+    expect(error).toBeFalsy()
   })
 })
