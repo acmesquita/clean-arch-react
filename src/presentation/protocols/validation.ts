953484230613d5edae00ee9fce39013ea14c58f8
Catharina Mesquita <catha.ana.1994@gmail.com>
2021-11-01 13:17:54 -0300

    refactor: update eslint plugins

--- src/presentation/protocols/validation.ts
+++ src/presentation/protocols/validation.ts
@@ -1,3 +1,3 @@
 export interface Validation {
-  validate(fieldName: string, fieldValeu: string): string | null
+  validate: (fieldName: string, fieldValeu: string) => string | null
 }
--- src/presentation/test/mock-authentication.ts
+++ src/presentation/test/mock-authentication.ts
@@ -10,6 +10,6 @@ export class AuthenticationSpy implements Authentication {
   async auth (params: AuthenticationParams): Promise<AccountModel> | null {
     this.params = params
     this.callsCount++
-    return Promise.resolve(this.account)
+    return this.account
   }
 }
