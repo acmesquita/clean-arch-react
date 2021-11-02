695a95560c30edb3416cba4a3466a5386376cc97
Catharina Mesquita <catha.ana.1994@gmail.com>
2021-11-02 13:44:27 -0300

    test: ensure CompareFieldsValiation return error if compare is invalid

--- src/validation/validators/compare-fields/compare-fields-validation.spec.ts
+++ src/validation/validators/compare-fields/compare-fields-validation.spec.ts
@@ -2,12 +2,13 @@ import faker from 'faker'
 import { InvalidFieldError } from '@/validation/errors'
 import { CompareFieldsValidation } from './compare-fields-validation'
 
-const fieldName = faker.database.column()
+const makeSut = (fieldName: string, valueToCompare: string): CompareFieldsValidation => new CompareFieldsValidation(fieldName, valueToCompare)
 
-const makeSut = (): CompareFieldsValidation => new CompareFieldsValidation(fieldName)
 describe('CompareFieldsValidation', () => {
-  test('Should return error if fields is empty', () => {
-    const sut = makeSut()
+  test('Should return error if compare is invalid', () => {
+    const fieldName = faker.database.column()
+    const valueToCompare = faker.random.word()
+    const sut = makeSut(fieldName, valueToCompare)
     const error = sut.validate(faker.random.word())
     expect(error).toEqual(new InvalidFieldError(fieldName))
   })
