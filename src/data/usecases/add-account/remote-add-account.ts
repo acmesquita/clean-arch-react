0f1e3821944752e1d763ed892503276b1725e1f9
Catharina Mesquita <catha.ana.1994@gmail.com>
2021-11-02 13:01:11 -0300

    feat: ensure RemoveAddAccount calls HttpPostClient with url and body correct

---
+++ src/data/usecases/add-account/remote-add-account.ts
@@ -0,0 +1,17 @@
+import { HttpPostClient, HttpStatusCode } from '@/data/protocols/http'
+import { UnexpectedError } from '@/domain/errors'
+import { AccountModel } from '@/domain/models'
+import { AddAccount, AddAccountParams } from '@/domain/usecases'
+
+export class RemoteAddAccount implements AddAccount {
+  constructor (private readonly url: string, private readonly httpPostClient: HttpPostClient<AddAccountParams, AccountModel>) {}
+
+  async add (params: AddAccountParams): Promise<AccountModel> {
+    const httpResponse = await this.httpPostClient.post({ url: this.url, body: params })
+
+    switch (httpResponse.statusCode) {
+      case HttpStatusCode.ok: return httpResponse.body
+      default: throw new UnexpectedError()
+    }
+  }
+}
