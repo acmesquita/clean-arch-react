1f86cb64da259dbeaa04d11b46d83e5f21acac36
Catharina Mesquita <catha.ana.1994@gmail.com>
2021-11-02 12:43:57 -0300

    refactor: move mocks http to a only file

--- src/data/test/mock-http-post-request.ts
+++
@@ -1,7 +0,0 @@
-import { HttpPostClientParams } from '@/data/protocols/http'
-import faker from 'faker'
-
-export const mockPostRequest = (): HttpPostClientParams<any> => ({
-  url: faker.internet.url(),
-  body: faker.random.word
-})
--- src/data/test/mock-http.ts
+++ src/data/test/mock-http.ts
@@ -1,3 +1,4 @@
+import faker from 'faker'
 import { HttpPostClient, HttpPostClientParams, HttpResponse, HttpStatusCode } from '@/data/protocols/http'
 
 export class HttpPostClientSpy<T, R> implements HttpPostClient<T, R> {
@@ -13,3 +14,8 @@ export class HttpPostClientSpy<T, R> implements HttpPostClient<T, R> {
     return this.response
   }
 }
+
+export const mockPostRequest = (): HttpPostClientParams<any> => ({
+  url: faker.internet.url(),
+  body: faker.random.word
+})
