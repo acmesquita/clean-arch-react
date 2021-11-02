f0b8ea631b6d110337f45ab4b09478e16ab2ccfb
Catharina Mesquita <catha.ana.1994@gmail.com>
2021-11-02 13:16:14 -0300

    test: ensure RemoteAddAccount return an AccountModel if HttpPostClient returns 200

--- src/data/usecases/add-account/remote-add-account.spec.ts
+++ src/data/usecases/add-account/remote-add-account.spec.ts
@@ -1,7 +1,7 @@
 import faker from 'faker'
 import { HttpPostClientSpy } from '@/data/test'
 import { AccountModel } from '@/domain/models'
-import { mockAddAccount } from '@/domain/test'
+import { mockAccountModel, mockAddAccount } from '@/domain/test'
 import { AddAccountParams } from '@/domain/usecases'
 import { RemoteAddAccount } from './remote-add-account'
 import { HttpStatusCode } from '@/data/protocols/http'
@@ -78,5 +78,17 @@ describe('RemoteAddAccount', () => {
     const promise = sut.add(mockAddAccount())
 
     await expect(promise).rejects.toThrow(new UnexpectedError())
+  })
+
+  test('Should return an AccountModel if HttpPostClient returns 200', async () => {
+    const { sut, httpPostClientSpy } = makeSut()
+    const httpResult = mockAccountModel()
+    httpPostClientSpy.response = {
+      statusCode: HttpStatusCode.ok,
+      body: httpResult
+    }
+    const account = await sut.add(mockAddAccount())
+
+    expect(account).toEqual(httpResult)
   })
 })
