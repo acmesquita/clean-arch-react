f38c1c107d7aee2d5b6ea13ee1e961a062afebf1
Catharina Mesquita <catha.ana.1994@gmail.com>
2021-11-08 18:12:35 -0300

    refactor: add input animations

--- src/presentation/components/input/index.tsx
+++ src/presentation/components/input/index.tsx
@@ -1,30 +1,40 @@
+import React, { useContext, useRef } from 'react'
 import { FormLoginContext } from '@/presentation/context/form/form-context'
-import React, { useContext } from 'react'
 import styles from './styles.scss'
 
 type Props = React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
 
 const Input: React.FC<Props> = (props: Props) => {
+  const inputRef = useRef<HTMLInputElement>(null)
   const { state, setState } = useContext(FormLoginContext)
-  const enableInput = (event: React.FocusEvent<HTMLInputElement>): void => {
-    event.target.readOnly = false
-  }
 
   const handleChange = (event: React.FocusEvent<HTMLInputElement>): void => {
-    setState({
-      ...state,
-      [event.target.name]: event.target.value
-    })
+    setState({ ...state, [event.target.name]: event.target.value })
   }
 
-  function getTitle (): string {
-    return state[`${props.name}Error`]
-  }
+  const error = state[`${props.name}Error`]
 
   return (
     <div className={styles.inputWrapper}>
-      <input {...props} readOnly onFocus={enableInput} data-testid={props.name} onChange={handleChange} />
-      <span title={getTitle()} data-testid={`${props.name}-status`} className={getTitle() ? styles.error : ''}></span>
+      <input
+        {...props}
+        placeholder=" "
+        readOnly
+        onFocus={event => { event.target.readOnly = false }}
+        data-testid={props.name}
+        onChange={handleChange}
+        ref={inputRef}
+      />
+      <label
+        onClick={() => inputRef.current.focus()}
+      >
+        {props.placeholder}
+      </label>
+      <span
+        title={error}
+        data-testid={`${props.name}-status`}
+        className={error ? styles.error : ''}
+      />
     </div>
 
   )
