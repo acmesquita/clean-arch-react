577fed08c5f5043539ec6b125a1afada88e81974
Catharina Mesquita <catha.ana.1994@gmail.com>
2021-11-08 15:52:41 -0300

    feat: add SignUpValidationFactory

---
+++ src/main/factories/pages/sign-up/sign-up-validation-factory.ts
@@ -0,0 +1,12 @@
+import { Validation } from '@/presentation/protocols/validation'
+import { ValidationComposite } from '@/validation/validators'
+import { ValidationBuilder as Builder } from '@/validation/validators/builder/validation-builder'
+
+export const makeSignUpValidation = (): Validation => (
+  ValidationComposite.build([
+    ...Builder.field('name').required().min(3).build(),
+    ...Builder.field('email').required().email().build(),
+    ...Builder.field('password').required().min(5).build(),
+    ...Builder.field('passwordConfirmation').required().min(5).build()
+  ])
+)
