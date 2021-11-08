7487063351c2f9869387c1a26505e27c6d2a6179
Catharina Mesquita <catha.ana.1994@gmail.com>
2021-11-08 17:19:58 -0300

    test: ensure Login present error state if form is invalid

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -1,15 +1,24 @@
+import faker from 'faker'
+
 describe('Login', () => {
   beforeEach(() => {
     cy.visit('login')
   })
 
   it('Shuld load with correct initial state', () => {
-    cy.getByTestId('email-status')
-      .should('have.attr', 'title', 'Campo obrigatório')
-      .should('not.have.class', 'error')
-    cy.getByTestId('password-status')
-      .should('have.attr', 'title', 'Campo obrigatório')
-      .should('not.have.class', 'error')
+    cy.getByTestId('email').should('have.attr', 'readOnly')
+    cy.getByTestId('email-status').should('have.attr', 'title', 'Campo obrigatório')
+    cy.getByTestId('password').should('have.attr', 'readOnly')
+    cy.getByTestId('password-status').should('have.attr', 'title', 'Campo obrigatório')
+    cy.getByTestId('submit').should('have.attr', 'disabled')
+    cy.getByTestId('error-wrapper').should('not.have.descendants')
+  })
+
+  it('Shuld present error state if form is invalid', () => {
+    cy.getByTestId('email').focus().type(faker.random.word())
+    cy.getByTestId('email-status').should('have.attr', 'title', "O campo 'email' está com valor inválido")
+    cy.getByTestId('password').focus().type(faker.random.alphaNumeric(3))
+    cy.getByTestId('password-status').should('have.attr', 'title', 'Tamanho inválido')
     cy.getByTestId('submit').should('have.attr', 'disabled')
     cy.getByTestId('error-wrapper').should('not.have.descendants')
   })
