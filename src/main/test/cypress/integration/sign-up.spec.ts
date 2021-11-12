065b02da8227e04476db31017f2a7284326b2403
Catharina Mesquita <catha.ana.1994@gmail.com>
2021-11-12 11:08:51 -0300

    test: ensure SignUp load with correct initial state

---
+++ src/main/test/cypress/integration/sign-up.spec.ts
@@ -0,0 +1,22 @@
+import * as FormHelper from '../suport/form-helper'
+
+describe('SignUp', () => {
+  it('Should load with correct initial state', () => {
+    cy.visit('signup')
+
+    cy.getByTestId('name').should('have.attr', 'readOnly')
+    FormHelper.testInputStatus('name', 'Campo obrigatório')
+
+    cy.getByTestId('email').should('have.attr', 'readOnly')
+    FormHelper.testInputStatus('email', 'Campo obrigatório')
+
+    cy.getByTestId('password').should('have.attr', 'readOnly')
+    FormHelper.testInputStatus('password', 'Campo obrigatório')
+
+    cy.getByTestId('passwordConfirmation').should('have.attr', 'readOnly')
+    FormHelper.testInputStatus('passwordConfirmation', 'Campo obrigatório')
+
+    cy.getByTestId('submit').should('have.attr', 'disabled')
+    cy.getByTestId('error-wrapper').should('not.have.descendants')
+  })
+})
