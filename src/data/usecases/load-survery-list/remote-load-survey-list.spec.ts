94b030990ed8e75dd6b4de6b201db3de1a57f6bb
Catharina Mesquita <catha.ana.1994@gmail.com>
2021-11-17 17:13:00 -0300

    test: ensure RemoteLoadSurveyList throws UnexpectedError if HttpGetClient returns 500

--- src/data/usecases/load-survery-list/remote-load-survey-list.spec.ts
+++ src/data/usecases/load-survery-list/remote-load-survey-list.spec.ts
@@ -48,4 +48,14 @@ describe('RemoteLoadSurveyList', () => {
 
     await expect(promise).rejects.toThrow(new UnexpectedError())
   })
+
+  test('Should throws UnexpectedError if HttpGetClient returns 500', async () => {
+    const { sut, httpGetClientSpy } = makeSut()
+    httpGetClientSpy.response = {
+      statusCode: HttpStatusCode.serverError
+    }
+    const promise = sut.loadAll()
+
+    await expect(promise).rejects.toThrow(new UnexpectedError())
+  })
 })
