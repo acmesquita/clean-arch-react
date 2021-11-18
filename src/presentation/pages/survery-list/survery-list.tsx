c6b15af396cbd2d36ac2390926b90b47459e9112
Catharina Mesquita <catha.ana.1994@gmail.com>
2021-11-18 11:35:00 -0300

    feat: add Survery icon to show status

--- src/presentation/pages/survery-list/survery-list.tsx
+++ src/presentation/pages/survery-list/survery-list.tsx
@@ -19,28 +19,9 @@ const SurveryList: React.FC = () => {
         <ul>
           <li>
             <div className={styles.surveryContent}>
-              <time>
-                <span className={styles.day}>18</span>
-                <span className={styles.month}>11</span>
-                <span className={styles.year}>2020</span>
-              </time>
-              <p>Qual é o seu framework web favorito?</p>
-            </div>
-            <footer>Ver Resultado</footer>
-          </li>
-          <li>
-            <div className={styles.surveryContent}>
-              <time>
-                <span className={styles.day}>18</span>
-                <span className={styles.month}>11</span>
-                <span className={styles.year}>2020</span>
-              </time>
-              <p>Qual é o seu framework web favorito?</p>
-            </div>
-            <footer>Ver Resultado</footer>
-          </li>
-          <li>
-            <div className={styles.surveryContent}>
+              <div className={[styles.iconWrapper, styles.green].join(' ')}>
+                <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAASCAYAAABb0P4QAAAAAXNSR0IArs4c6QAAAERlWElmTU0AKgAAAAgAAYdpAAQAAAABAAAAGgAAAAAAA6ABAAMAAAABAAEAAKACAAQAAAABAAAAFKADAAQAAAABAAAAEgAAAAA9nQVdAAAA0klEQVQ4EWNgIAH8//+/AYhLSNCCWynUMCD1/zcQG+BWSYQMkmEgA0Egjght2JUANYO8iQ4MsasmIAo0BZthP4DirAS0YkrjMAzk0tOYqgmIADUVgnTiADPxakfStAWmECj2DkmcWOYjoEJPRpBqmEGMQABiI4vB5IikH1PbQAYmIm0mVtlLahu4nJpe/gf0hho1XbgVGKd3qWngRFBA4/LyX6AcKZZdBbpOB2QgLk1nQJIkgElwtaBEDAXIOUULKHYSiP/CJHHQX4Hic4CYBWYgADx8PyqFiuhJAAAAAElFTkSuQmCC" alt="up"/>
+              </div>
               <time>
                 <span className={styles.day}>18</span>
                 <span className={styles.month}>11</span>
@@ -58,3 +39,5 @@ const SurveryList: React.FC = () => {
 }
 
 export default SurveryList
+
+// <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAASCAYAAABb0P4QAAAAAXNSR0IArs4c6QAAAERlWElmTU0AKgAAAAgAAYdpAAQAAAABAAAAGgAAAAAAA6ABAAMAAAABAAEAAKACAAQAAAABAAAAFKADAAQAAAABAAAAEgAAAAA9nQVdAAAA70lEQVQ4Ea2RPQoCQQyFZ/w5g72lYOEVPIiV2IkIHmCvIZ5D77BgZWtrYWe1ICiuL8tEwjIZZmYNZCf7knyTzRrjrK7rAfwAr+AheyNZwiei98gNrBkISxYjz5KbZb0V4gXxlN8jzo+1tk91BOT6nhPmOFNg1Nb0UiCNxY0Uu8QW044BuMIZHs3DJzcra3/yOgem3UoT3pEcaQUh3TchAX9/KNTsy/mAtLebrzhXI+AqE/oQl55ErIfYxp5WothW71QyAJ0VWKG06DJAQ/jTA0yH0TUAzf4Gc8BFC5g3GcHI3IQvBy0asesDsB08CfYFB/44kX6+Hj8AAAAASUVORK5CYII=" alt="down" />
