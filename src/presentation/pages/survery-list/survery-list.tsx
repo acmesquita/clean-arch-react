ba62cba92f40542cffd0a6ee076d45b3b419c3bf
Catharina Mesquita <catha.ana.1994@gmail.com>
2021-11-18 11:55:00 -0300

    refactor: move icon to its own file

--- src/presentation/pages/survery-list/survery-list.tsx
+++ src/presentation/pages/survery-list/survery-list.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import styles from './styles.scss'
-import { Footer, Header } from '@/presentation/components'
+import { Footer, Header, Icon } from '@/presentation/components'
+import { IconName } from '@/presentation/components/icon'
 
 const SurveryList: React.FC = () => {
   return (
@@ -11,9 +12,7 @@ const SurveryList: React.FC = () => {
         <ul>
           <li>
             <div className={styles.surveryContent}>
-              <div className={[styles.iconWrapper, styles.green].join(' ')}>
-                <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAASCAYAAABb0P4QAAAAAXNSR0IArs4c6QAAAERlWElmTU0AKgAAAAgAAYdpAAQAAAABAAAAGgAAAAAAA6ABAAMAAAABAAEAAKACAAQAAAABAAAAFKADAAQAAAABAAAAEgAAAAA9nQVdAAAA0klEQVQ4EWNgIAH8//+/AYhLSNCCWynUMCD1/zcQG+BWSYQMkmEgA0Egjght2JUANYO8iQ4MsasmIAo0BZthP4DirAS0YkrjMAzk0tOYqgmIADUVgnTiADPxakfStAWmECj2DkmcWOYjoEJPRpBqmEGMQABiI4vB5IikH1PbQAYmIm0mVtlLahu4nJpe/gf0hho1XbgVGKd3qWngRFBA4/LyX6AcKZZdBbpOB2QgLk1nQJIkgElwtaBEDAXIOUULKHYSiP/CJHHQX4Hic4CYBWYgADx8PyqFiuhJAAAAAElFTkSuQmCC" alt="up"/>
-              </div>
+              <Icon iconName={IconName.thumbUp} className={styles.iconWrapper}/>
               <time>
                 <span className={styles.day}>18</span>
                 <span className={styles.month}>11</span>
@@ -31,5 +30,3 @@ const SurveryList: React.FC = () => {
 }
 
 export default SurveryList
-
-// <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAASCAYAAABb0P4QAAAAAXNSR0IArs4c6QAAAERlWElmTU0AKgAAAAgAAYdpAAQAAAABAAAAGgAAAAAAA6ABAAMAAAABAAEAAKACAAQAAAABAAAAFKADAAQAAAABAAAAEgAAAAA9nQVdAAAA70lEQVQ4Ea2RPQoCQQyFZ/w5g72lYOEVPIiV2IkIHmCvIZ5D77BgZWtrYWe1ICiuL8tEwjIZZmYNZCf7knyTzRrjrK7rAfwAr+AheyNZwiei98gNrBkISxYjz5KbZb0V4gXxlN8jzo+1tk91BOT6nhPmOFNg1Nb0UiCNxY0Uu8QW044BuMIZHs3DJzcra3/yOgem3UoT3pEcaQUh3TchAX9/KNTsy/mAtLebrzhXI+AqE/oQl55ErIfYxp5WothW71QyAJ0VWKG06DJAQ/jTA0yH0TUAzf4Gc8BFC5g3GcHI3IQvBy0asesDsB08CfYFB/44kX6+Hj8AAAAASUVORK5CYII=" alt="down" />
