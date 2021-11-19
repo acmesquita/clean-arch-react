39e1b5c220b9ae0da65d9f15e2ee0a565b576355
Catharina Mesquita <catha.ana.1994@gmail.com>
2021-11-19 14:58:50 -0300

    feat: add getCurrentAccount to ApiContext

--- src/presentation/context/api/api-context.tsx
+++ src/presentation/context/api/api-context.tsx
@@ -3,6 +3,7 @@ import { AccountModel } from '@/domain/models'
 
 type Props = {
   setCurrentAccount?: (account: AccountModel) => void
+  getCurrentAccount?: () => AccountModel
 }
 
 export const ApiContext = createContext<Props>(null)
