ff46e4d5bdf6a1fec8830f8cba2ac7fa0b2a3115
Catharina Mesquita <catha.ana.1994@gmail.com>
2021-11-22 15:51:16 -0300

    refactor: move SUT creation to a factory method

--- src/presentation/pages/survery-list/survery-list.spec.tsx
+++ src/presentation/pages/survery-list/survery-list.spec.tsx
@@ -2,9 +2,13 @@ import React from 'react'
 import { render, screen } from '@testing-library/react'
 import { SurveryList } from '@/presentation/pages'
 
+const makeSut = (): void => {
+  render(<SurveryList />)
+}
+
 describe('SurveryList Component', () => {
   test('Should present 4 empyt SurveryItemEmpty components on start', () => {
-    render(<SurveryList />)
+    makeSut()
     const surveryList = screen.getByTestId('survery-list')
     expect(surveryList.querySelectorAll('li:empty').length).toBe(4)
   })
