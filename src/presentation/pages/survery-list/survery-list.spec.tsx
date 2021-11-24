74fcbefcd0c9599324bf5183b468152426024ee0
Catharina Mesquita <catha.ana.1994@gmail.com>
2021-11-24 11:28:27 -0300

    test: ensure SurveryList render SurveryItems on success

--- src/presentation/pages/survery-list/survery-list.spec.tsx
+++ src/presentation/pages/survery-list/survery-list.spec.tsx
@@ -1,14 +1,17 @@
 import React from 'react'
-import { render, screen } from '@testing-library/react'
+import { render, screen, waitFor } from '@testing-library/react'
 import { SurveryList } from '@/presentation/pages'
 import { LoadSurveryList } from '@/domain/usecases'
 import { SurveryModel } from '@/domain/models'
+import { mockSurveryListModel } from '@/domain/test'
 
 class LoadSurveryListSpy implements LoadSurveryList {
   callsCount = 0
+  surveries = mockSurveryListModel()
+
   async loadAll (): Promise<SurveryModel[]> {
     this.callsCount++
-    return null
+    return this.surveries
   }
 }
 
@@ -26,14 +29,23 @@ const makeSut = (): SutType => {
 }
 
 describe('SurveryList Component', () => {
-  test('Should present 4 empyt SurveryItemEmpty components on start', () => {
+  test('Should present 4 empyt SurveryItemEmpty components on start', async () => {
     makeSut()
     const surveryList = screen.getByTestId('survery-list')
-    expect(surveryList.querySelectorAll('li:empty').length).toBe(4)
+    expect(surveryList.querySelectorAll('li:empty')).toHaveLength(4)
+    await waitFor(() => surveryList)
   })
 
-  test('Should call LoadSurveryList', () => {
+  test('Should call LoadSurveryList', async () => {
     const { loadSurveryListSpy } = makeSut()
     expect(loadSurveryListSpy.callsCount).toBe(1)
+    await waitFor(() => screen.getByRole('heading'))
+  })
+
+  test('Should render SurveryItems on success', async () => {
+    makeSut()
+    const surveryList = screen.getByTestId('survery-list')
+    await waitFor(() => surveryList)
+    expect(surveryList.querySelectorAll('li.surveryItemWrapper')).toHaveLength(2)
   })
 })
