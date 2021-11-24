3255c4749e5cc8b86be741152f283d2372c92b9b
Catharina Mesquita <catha.ana.1994@gmail.com>
2021-11-24 10:01:03 -0300

    feat: ensure SurveryList Page calls LoadSurveryList

--- src/presentation/pages/survery-list/survery-list.tsx
+++ src/presentation/pages/survery-list/survery-list.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import styles from './styles.scss'
 import { Footer, Header } from '@/presentation/components'
 import { SurveryItem, SurveryItemEmpty } from '@/presentation/pages/survery-list/components'
+import { LoadSurveryList } from '@/domain/usecases'
 
-const SurveryList: React.FC = () => {
+type Props = {
+  loadSurveryList: LoadSurveryList
+}
+
+const SurveryList: React.FC<Props> = ({ loadSurveryList }: Props) => {
+  useEffect(() => {
+    (async () => {
+      await loadSurveryList.loadAll()
+    })()
+  }, [])
+
   return (
     <div className={styles.surveyWrapper}>
       <Header />
