2e34a2cae0c243e6958013deb75947078a54b6f0
Catharina Mesquita <catha.ana.1994@gmail.com>
2021-11-24 11:27:59 -0300

    feat: ensure SurveryList render SurveryItems on success

--- src/presentation/pages/survery-list/survery-list.tsx
+++ src/presentation/pages/survery-list/survery-list.tsx
@@ -1,18 +1,22 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from './styles.scss'
 import { Footer, Header } from '@/presentation/components'
 import { SurveryItem, SurveryItemEmpty } from '@/presentation/pages/survery-list/components'
 import { LoadSurveryList } from '@/domain/usecases'
+import { SurveryModel } from '@/domain/models'
 
 type Props = {
   loadSurveryList: LoadSurveryList
 }
 
 const SurveryList: React.FC<Props> = ({ loadSurveryList }: Props) => {
+  const [surveries, setSurveries] = useState<SurveryModel[]>([])
+
   useEffect(() => {
-    (async () => {
-      await loadSurveryList.loadAll()
-    })()
+    loadSurveryList.loadAll()
+      .then(result => {
+        setSurveries(result)
+      })
   }, [])
 
   return (
@@ -21,7 +25,13 @@ const SurveryList: React.FC<Props> = ({ loadSurveryList }: Props) => {
       <main className={styles.contentWrapper}>
         <h2>Enquetes</h2>
         <ul data-testid="survery-list">
-          <SurveryItemEmpty />
+          { surveries.length > 0
+            ? (
+                surveries.map(survery => (<SurveryItem key={survery.id} survery={survery} />))
+              )
+            : (
+            <SurveryItemEmpty />
+              )}
         </ul>
       </main>
       <Footer />
