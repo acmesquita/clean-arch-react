198c74104b9a044cebf34b86ede660a06c5c0e06
Catharina Mesquita <catha.ana.1994@gmail.com>
2021-11-25 18:44:07 -0300

    feat: add AuthorizeHttpGetClientDecorator factory

--- src/main/factories/usecases/load-survey-list/remote-load-survey-list-factory.ts
+++ src/main/factories/usecases/load-survey-list/remote-load-survey-list-factory.ts
@@ -1,7 +1,8 @@
+import { makeApiUrlFactory } from '@/main/factories/http'
+import { makeAuthorizeHttpGetClientDecorator } from '@/main/factories/decorators'
 import { LoadSurveyList } from '@/domain/usecases'
 import { RemoteLoadSurveyList } from '@/data/usecases'
-import { makeAxiosHttpClient, makeApiUrlFactory } from '@/main/factories/http'
 
 export const makeRemoveLoadSurveyList = (): LoadSurveyList => {
-  return new RemoteLoadSurveyList(makeApiUrlFactory('/surveys'), makeAxiosHttpClient())
+  return new RemoteLoadSurveyList(makeApiUrlFactory('/surveys'), makeAuthorizeHttpGetClientDecorator())
 }
