6a2eb20a24be229baa2d4aa20f13f5316a2f9b44
Catharina Mesquita <catha.ana.1994@gmail.com>
2021-11-26 18:07:41 -0300

    refactor: change validation to not use biulder

--- src/main/factories/pages/sign-up/sign-up-validation-factory.spec.ts
+++ src/main/factories/pages/sign-up/sign-up-validation-factory.spec.ts
@@ -1,15 +1,19 @@
-import { ValidationComposite } from '@/validation/validators'
-import { ValidationBuilder as Builder } from '@/validation/validators/builder/validation-builder'
+import { CompareFieldsValidation, EmailValidation, MinLenghtValidation, RequiredFieldValidation, ValidationComposite } from '@/validation/validators'
 import { makeSignUpValidation } from '@/main/factories/pages'
 
 describe('SignUpValidationFactory', () => {
   test('Should make ValidationComposite with correct validations', () => {
     const composite = makeSignUpValidation()
     expect(composite).toEqual(ValidationComposite.build([
-      ...Builder.field('name').required().min(3).build(),
-      ...Builder.field('email').required().email().build(),
-      ...Builder.field('password').required().min(5).build(),
-      ...Builder.field('passwordConfirmation').required().min(5).sameAs('password').build()
+      new RequiredFieldValidation('name'),
+      new MinLenghtValidation('name', 3),
+      new RequiredFieldValidation('email'),
+      new EmailValidation('email'),
+      new RequiredFieldValidation('password'),
+      new MinLenghtValidation('password', 5),
+      new RequiredFieldValidation('passwordConfirmation'),
+      new MinLenghtValidation('passwordConfirmation', 5),
+      new CompareFieldsValidation('passwordConfirmation', 'password')
     ]))
   })
 })
