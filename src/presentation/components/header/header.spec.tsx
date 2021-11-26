6a4688fadcb8c47107ce6696ce8f8f91b5107bee
Catharina Mesquita <catha.ana.1994@gmail.com>
2021-11-26 12:02:34 -0300

    refactor: move SUT creation to a factory method

--- src/presentation/components/header/header.spec.tsx
+++ src/presentation/components/header/header.spec.tsx
@@ -1,21 +1,36 @@
 import React from 'react'
 import { render, screen, fireEvent } from '@testing-library/react'
 import { Router } from 'react-router-dom'
-import { createMemoryHistory } from 'history'
+import { createMemoryHistory, MemoryHistory } from 'history'
 import { Header } from '@/presentation/components'
 import { ApiContext } from '@/presentation/context'
+import { AccountModel } from '@/domain/models'
+
+type SutType = {
+  setCurrentAccountMock: (account: AccountModel) => void
+  history: MemoryHistory
+}
+
+const makeSut = (): SutType => {
+  const setCurrentAccountMock = jest.fn()
+  const history = createMemoryHistory({ initialEntries: ['/'] })
+  render(
+    <Router history={history}>
+      <ApiContext.Provider value={{ setCurrentAccount: setCurrentAccountMock }}>
+        <Header />
+      </ApiContext.Provider>
+    </Router>
+  )
+
+  return {
+    setCurrentAccountMock,
+    history
+  }
+}
 
 describe('Header Component', () => {
   test('Should call setCurrentAccount with null', () => {
-    const setCurrentAccountMock = jest.fn()
-    const history = createMemoryHistory({ initialEntries: ['/'] })
-    render(
-       <Router history={history}>
-        <ApiContext.Provider value={{ setCurrentAccount: setCurrentAccountMock }}>
-          <Header />
-        </ApiContext.Provider>
-       </Router>
-    )
+    const { setCurrentAccountMock, history } = makeSut()
     fireEvent.click(screen.getByTestId('logout'))
     expect(setCurrentAccountMock).toHaveBeenCalledWith(undefined)
 
