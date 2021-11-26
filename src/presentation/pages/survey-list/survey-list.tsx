05fe099f5f5e4ea73326a6c3bc543d44c967811a
Catharina Mesquita <catha.ana.1994@gmail.com>
2021-11-26 18:26:15 -0300

    refactor: change SurveryList to use custom hook

--- src/presentation/pages/survey-list/survey-list.tsx
+++ src/presentation/pages/survey-list/survey-list.tsx
@@ -1,34 +1,26 @@
-import React, { useContext, useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from './styles.scss'
 import { Footer, Header } from '@/presentation/components'
 import { List, SurveyContext, SurveyError } from '@/presentation/pages/survey-list/components'
 import { LoadSurveyList } from '@/domain/usecases'
-import { AccessDeniedError } from '@/domain/errors'
-import { useHistory } from 'react-router'
-import { ApiContext } from '@/presentation/context'
+import { useErrorHandler } from '@/presentation/hooks'
 
 type Props = {
   loadSurveyList: LoadSurveyList
 }
 
 const SurveyList: React.FC<Props> = ({ loadSurveyList }: Props) => {
-  const history = useHistory()
-  const { setCurrentAccount } = useContext(ApiContext)
   const [surveys, setSurveys] = useState<LoadSurveyList.Model[]>([])
-  const [error, setError] = useState()
+  const [error, setError] = useState('')
   const [reload, setReload] = useState(false)
+  const handleError = useErrorHandler((err: Error) => {
+    setError(err.message)
+  })
 
   useEffect(() => {
     loadSurveyList.loadAll()
       .then(result => setSurveys(result))
-      .catch(error => {
-        if (error instanceof AccessDeniedError) {
-          setCurrentAccount(undefined)
-          history.replace('/login')
-        } else {
-          setError(error.message)
-        }
-      })
+      .catch(handleError)
   }, [reload])
 
   return (
