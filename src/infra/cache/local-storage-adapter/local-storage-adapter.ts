5c76a122e418158e61b17640ccc513ca085f871e
Catharina Mesquita <catha.ana.1994@gmail.com>
2021-11-26 10:59:12 -0300

    feat: ensure LocalStorageAdapter#set calls remove itens if value is null

--- src/infra/cache/local-storage-adapter/local-storage-adapter.ts
+++ src/infra/cache/local-storage-adapter/local-storage-adapter.ts
@@ -2,7 +2,11 @@ import { SetStorage, GetStorage } from '@/data/protocols/cache'
 
 export class LocalStorageAdapter implements SetStorage, GetStorage {
   set (key: string, value: object): void {
-    localStorage.setItem(key, JSON.stringify(value))
+    if (!value) {
+      localStorage.removeItem(key)
+    } else {
+      localStorage.setItem(key, JSON.stringify(value))
+    }
   }
 
   get (key: string): any {
