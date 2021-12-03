544ea6f16e1b1d26f7c4693417a232c388c25448
Catharina Mesquita <catha.ana.1994@gmail.com>
2021-12-03 18:18:32 -0300

    chore: add webpack prod and dev configs

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,21 +1,11 @@
-const path = require('path')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const { DefinePlugin } = require('webpack')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const { merge } = require('webpack-merge')
 
-module.exports = {
+const common = require('./webpack.common')
+
+module.exports = merge(common, {
   mode: 'development',
-  entry: './src/main/index.tsx',
-  output: {
-    path: path.join(__dirname, 'public/js'),
-    publicPath: '/public/js',
-    filename: 'bundle.js'
-  },
-  resolve: {
-    extensions: ['.ts', '.tsx', 'scss', '.js'],
-    alias: {
-      '@': path.join(__dirname, 'src')
-    }
-  },
   module: {
     rules: [
       {
@@ -39,19 +29,18 @@ module.exports = {
       writeToDisk: true
     },
     static: {
-      directory: './public'
+      directory: './dist'
     },
     historyApiFallback: true,
     port: 8080
   },
-  externals: {
-    react: 'React',
-    'react-dom': 'ReactDOM'
-  },
   plugins: [
-    new CleanWebpackPlugin(),
     new DefinePlugin({
       'process.env.API_URL': JSON.stringify('https://fordevs.herokuapp.com/api')
+    }),
+    new HTMLWebpackPlugin({
+      filename: 'index.html',
+      template: './template.dev.html'
     })
   ]
-}
+})
---
+++ webpack.prod.js
@@ -0,0 +1,44 @@
+const { DefinePlugin } = require('webpack')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const { merge } = require('webpack-merge')
+
+const common = require('./webpack.common')
+
+module.exports = merge(common, {
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.ts(x?)$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          { loader: MiniCssExtractPlugin.loader },
+          { loader: 'css-loader', options: { modules: true } },
+          { loader: 'sass-loader' }
+        ],
+        exclude: /node_modules/
+      }
+    ]
+  },
+  externals: {
+    react: 'React',
+    'react-dom': 'ReactDOM'
+  },
+  plugins: [
+    new DefinePlugin({
+      'process.env.API_URL': JSON.stringify('https://fordevs.herokuapp.com/api')
+    }),
+    new HTMLWebpackPlugin({
+      filename: 'index.html',
+      template: './template.prod.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'main-bundle-[hash].css'
+    })
+  ]
+})
